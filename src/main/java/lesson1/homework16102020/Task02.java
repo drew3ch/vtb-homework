ca3442794e2bc490d5ce73e25b1f680b55738350
drewprogrammer <uEawcxOk5QrERJvFCWd0>
2020-10-14 18:45:05 +0300

    Homework 14-16.10.2020 Andrew Chernov

---
+++ src/main/java/lesson1/homework16102020/Task02.java
@@ -0,0 +1,30 @@
+package lesson1.homework16102020;
+
+import java.util.Arrays;
+
+public class Task02 {
+
+    public static void main(String[] args) {
+
+        int[] intArr = {10, 1, 30, 4, 5, 6};
+        sort(intArr);
+
+        System.out.println(Arrays.toString(intArr));
+
+    }
+
+    public static void sort(int[] arr) {
+        for (int min = 0; min < arr.length; min++) {
+            int l = min;
+            for (int j = min + 1; j < arr.length; j++) {
+                if (arr[j] < arr[l]) {
+                    l = j;
+                }
+            }
+            int tmp = arr[min];
+            arr[min] = arr[l];
+            arr[l] = tmp;
+        }
+    }
+
+}
