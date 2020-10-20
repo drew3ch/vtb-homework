94d02eac3cefc0d2ad9c25cba899a75fb8d7a5b6
drewprogrammer <uEawcxOk5QrERJvFCWd0>
2020-10-20 18:25:06 +0300

    Homework 21.10.2020-3

--- src/main/java/lesson1/homework21102020/Game.java
+++ src/main/java/lesson1/homework21102020/Game.java
@@ -41,35 +41,35 @@ public class Game {
     }
 
     private void startGame() {
-        boolean isNoWin = true;
+        boolean isWin = true;
         for (Player player : players) {
             for(int i = 0; i < MAX_BARRIERS; i++) {
                 System.out.println(player.getName() + ": " + treadmills[i].getName());
                 if(player.getRun() >= treadmills[i].getMeters()) {
                     System.out.println(player.getName() + " успешно пробежал " + treadmills[i].getMeters() + " метров.");
                     player.setRun(player.getRun() - treadmills[i].getMeters());
-                    isNoWin = true;
+                    isWin = true;
                 } else if(player.getRun() < treadmills[i].getMeters()) {
                     System.out.println(player.getName() + " не смог бежать.");
                     System.out.println("----------");
                     player.setRun(0);
-                    isNoWin = false;
+                    isWin = false;
                     break;
                 }
                 System.out.println(player.getName() + ": " + walls[i].getName());
                 if (player.getJump() >= walls[i].getHeight()) {
                     System.out.println(player.getName() + " перепрыгнул успешно " + walls[i].getHeight() + " метров в высоту.");
                     player.setJump(player.getJump() - walls[i].getHeight());
-                    isNoWin = true;
+                    isWin = true;
                 } else if (player.getJump() < walls[i].getHeight()) {
                     System.out.println(player.getName() + " не смог перепрыгнуть.");
                     System.out.println("----------");
                     player.setJump(0);
-                    isNoWin = false;
+                    isWin = false;
                     break;
                 }
             }
-            if(isNoWin) {
+            if(isWin) {
                 System.out.println(player.getName() + " успешно прошел все испытания.");
                 System.out.println("----------");
             }
