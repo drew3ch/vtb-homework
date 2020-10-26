95f124472fd39c1493190093dbebe8292358525f
drewprogrammer <uEawcxOk5QrERJvFCWd0>
2020-10-26 23:28:38 +0300

    Homework 28.10.2020

---
+++ src/main/java/lesson1/homework28102020/Box.java
@@ -0,0 +1,41 @@
+package lesson1.homework28102020;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Box<T extends Fruit> {
+
+    private ArrayList<T> fruits;
+
+    public Box(T... fruits) {
+        this.fruits = new ArrayList<T>(Arrays.asList(fruits));
+    }
+
+    public void add(T... fruits) {
+        this.fruits.addAll(Arrays.asList(fruits));
+    }
+
+    public float getWeight() {
+
+        if (fruits.size() == 0) {
+            return 0;
+        } else {
+            return fruits.size() * fruits.get(0).getWeight();
+        }
+
+    }
+
+    public boolean compare(Box box) {
+
+        return this.getWeight() == box.getWeight();
+
+    }
+
+    public void transfer(Box<T> box) {
+
+        box.fruits.addAll(this.fruits);
+        fruits.clear();
+
+    }
+
+}
