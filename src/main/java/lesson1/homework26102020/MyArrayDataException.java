f29b1b88aaa229a9350cb8ec3d11c3db99040111
drewprogrammer <uEawcxOk5QrERJvFCWd0>
2020-10-26 09:42:32 +0300

    Homework 26.10.2020-5

--- src/main/java/lesson1/homework26102020/MyArrayDataException.java
+++ src/main/java/lesson1/homework26102020/MyArrayDataException.java
@@ -3,7 +3,7 @@ package lesson1.homework26102020;
 public class MyArrayDataException extends RuntimeException {
 
     public MyArrayDataException(int i, int j, String value) {
-        super("Cell [" + i + "x" + j + "] contains a non-int type with value is '" + value + "'");
+        super(String.format("Cell [%dx%d] contains a non-int type with value is '%s'", i, j, value));
     }
 
 }
