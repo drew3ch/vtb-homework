93312d44dc2a43cf65153dc939c89fe2ae6aac61
drewprogrammer <uEawcxOk5QrERJvFCWd0>
2020-11-02 23:58:50 +0300

    Homework 02.11.2020-10

--- src/main/java/lesson1/homework02112020/countRepeatsInFile.java
+++ src/main/java/lesson1/homework02112020/countRepeatsInFile.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class countRepeatsInFile {
 
@@ -33,7 +32,7 @@ public class countRepeatsInFile {
 
     public static void main(String args[]) throws IOException {
 
-        System.out.println(countRepeatsInFile(Paths.get("C:\\file.txt"), "aaa"));
+        System.out.println(countRepeatsInFile(Path.of("C:\\file.txt"), "aaa"));
 
     }
 }
