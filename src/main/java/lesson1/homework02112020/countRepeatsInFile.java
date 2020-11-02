cea398a9b0986dd8f81da5a853c95b48b823102d
drewprogrammer <uEawcxOk5QrERJvFCWd0>
2020-11-02 23:27:53 +0300

    Homework 02.11.2020-9

--- src/main/java/lesson1/homework02112020/countRepeatsInFile.java
+++ src/main/java/lesson1/homework02112020/countRepeatsInFile.java
@@ -2,6 +2,7 @@ package lesson1.homework02112020;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,24 +10,25 @@ import java.nio.file.Paths;
 public class countRepeatsInFile {
 
     public static int countRepeatsInFile(Path path, String sequence) throws IOException {
-        BufferedReader reader = Files.newBufferedReader(path);
-        StringBuilder sb = new StringBuilder();
-        int n = 0;
-        int nextChar;
-        while ((nextChar = reader.read()) != -1) {
-            if (sb.length() < sequence.length()) {
+        try (BufferedReader  reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            StringBuilder sb = new StringBuilder();
+            int n = 0;
+            int nextChar;
+            while ((nextChar = reader.read()) != -1) {
+                if (sb.length() < sequence.length()) {
+                    sb.append((char) nextChar);
+                    continue;
+                } else if (sb.toString().equals(sequence)) {
+                    n++;
+                }
+                sb.deleteCharAt(0);
                 sb.append((char) nextChar);
-                continue;
-            } else if (sb.toString().equals(sequence)) {
+            }
+            if (sb.toString().equals(sequence)) {
                 n++;
             }
-            sb.deleteCharAt(0);
-            sb.append((char) nextChar);
+            return n;
         }
-        if (sb.toString().equals(sequence)) {
-            n++;
-        }
-        return n;
     }
 
     public static void main(String args[]) throws IOException {
